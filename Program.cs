97659ed1e0b87edb4b06743a9ce1caa4417e3602
Regina <regina-22.87@mail.ru>
2023-01-29 00:29:29 +0500

    Ввод количества элеменов массива

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+
+
+int ElementsCount=ReadInt("Введите количество элементов массива: ");
