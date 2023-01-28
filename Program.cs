6cabd9b9166d19a83b447533069c82e374ea00a2
Regina <regina-22.87@mail.ru>
2023-01-29 00:31:19 +0500

    Заполнение массива

--- Program.cs
+++ Program.cs
@@ -5,9 +5,20 @@ int ReadInt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
+string [] Zapolnenie(int count)
+{
+string[] array = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент массива под индексом {i}:  ");
+        array[i]=Console.ReadLine();
+    }
+    return array;
+}
 
 
 
 
 
 int ElementsCount=ReadInt("Введите количество элементов массива: ");
+string [] array = Zapolnenie(ElementsCount);
